84a54c55a4526618b98116ff99bbd8f9331f0e4c
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-07 12:16:04 -0300

    feat: Adiciona classes categoria e produto

---
+++ FoodApp.Menu/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodApp.Menu.Models
+{
+    public class Category
+    {
+        public int             Id       { get; set; }
+
+        public string?         Name     { get; set; }
+
+        public IList<Product>? Products { get; set; }
+    }
+}
---
+++ FoodApp.Menu/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace FoodApp.Menu.Models;
+
+public class Product
+{
+    private int            Id          { get; set; }
+                           
+    private string?        Name        { get; set; }
+                           
+    private string?        Description { get; set; }
+                                                 
+    private string?        Price       { get; set; }
+                          
+    private IList<string>? Items       { get; set; }
+
+    private Category?      Category    { get; set; }
+
+    private int?           CategoryId  { get; set; }
+
+
+
+}
