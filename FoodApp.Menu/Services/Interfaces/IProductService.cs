70ee577259b48a192212656b6faa1848c40d989d
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 14:36:04 -0300

    feat: Adiciona Product service e sua interface

---
+++ FoodApp.Menu/Services/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using FoodApp.Menu.DTOs;
+
+namespace FoodApp.Menu.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductDTO> Register(ProductDTO productDTO);
+
+        Task<ProductDTO> Update(ProductDTO productDTO);
+
+        Task<ProductDTO> Delete(int id);
+
+        Task<ProductDTO> FindById(int productId);
+
+        Task<ProductDTO> FindByName(string productName);
+
+        Task<IList<ProductDTO>> FindAll();
+    }
+}
---
+++ FoodApp.Menu/Services/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using FoodApp.Menu.DTOs;
+using FoodApp.Menu.Models;
+using FoodApp.Menu.Repositories.Interfaces;
+using FoodApp.Menu.Services.Interfaces;
+
+namespace FoodApp.Menu.Services;
+
+public class ProductService : IProductService
+{
+    
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        this.mapper = mapper;
+    }
+
+    public async Task<ProductDTO> Delete(int id)
+    {
+        var entity = await _productRepository.Delete(id);
+        return mapper.Map<ProductDTO>(entity);
+    }
+
+    public async Task<IList<ProductDTO>> FindAll()
+    {
+        var entitylist = await _productRepository.GetAll();
+        return mapper.Map<IList<ProductDTO>>(entitylist);
+    }
+
+    public async Task<ProductDTO> FindById(int productId)
+    {
+        var entity = await _productRepository.GetById(productId);
+        return mapper.Map<ProductDTO>(entity);
+    }
+
+    public async Task<ProductDTO> FindByName(string productName)
+    {
+        var entity = await _productRepository.GetByName(productName);
+        return mapper.Map<ProductDTO>(entity);
+    }
+
+    public async Task<ProductDTO> Register(ProductDTO productDTO)
+    {
+        var entity = await _productRepository.Create(mapper.Map<Product>(productDTO));
+
+        return mapper.Map<ProductDTO>(entity);
+    }
+
+    public async Task<ProductDTO> Update(ProductDTO productDTO)
+    {
+        var entity = await _productRepository.Update(mapper.Map<Product>(productDTO));
+        return mapper.Map<ProductDTO>(entity);
+
+    }
+}
