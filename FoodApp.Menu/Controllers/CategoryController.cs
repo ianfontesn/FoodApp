a849411e5c66019308a3ccd458033ad402e03cae
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 17:46:21 -0300

    feat: Adiciona Category Controller

---
+++ FoodApp.Menu/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using FoodApp.Menu.DTOs;
+using FoodApp.Menu.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApp.Menu.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpPost]
+    [Route("Create")]
+    public async Task<ActionResult<CategoryDTO>> CreateCategory([FromBody] CategoryDTO dto)
+    {
+        try
+        {
+            return Ok(await _categoryService.Register(dto));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CategoryDTO>> GetAll()
+    {
+        try
+        {
+            return Ok(await _categoryService.FindAll());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<CategoryDTO>> Get(int id)
+    {
+        try
+        {
+            return Ok(await _categoryService.FindById(id));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<CategoryDTO>> Update([FromBody] CategoryDTO dto)
+    {
+        try
+        {
+            if (dto is null)
+            {
+                return BadRequest("DTO is null on Update Category");
+            }
+
+            return Ok(await _categoryService.Update(dto));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
