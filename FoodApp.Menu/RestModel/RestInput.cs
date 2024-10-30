1dd31e6b1e0b5fa69fa2f42fdd9e1c10c606fc53
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 14:33:52 -0300

    refact: Remove rest input e output

--- FoodApp.Menu/RestModel/RestInput.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FoodApp.Menu.RestModel
-{
-    public class RestInput<T> where T : class
-    {
-        public IList<string> Header { get; set; } = [];
-        public required T Body { get; set; }
-    }
-}
