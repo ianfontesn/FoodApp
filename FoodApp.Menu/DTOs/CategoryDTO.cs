10b57515eafab044ee33adf388c9987687d105f2
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 14:29:26 -0300

    feat: Cria category e product DTOs

---
+++ FoodApp.Menu/DTOs/CategoryDTO.cs
@@ -0,0 +1,20 @@
+﻿using FoodApp.Menu.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodApp.Menu.DTOs
+{
+    public class CategoryDTO
+    {
+        [Required(ErrorMessage = "The name is required.")]
+        [MinLength(3), MaxLength(200)]
+        public string? Name { get; set; } = String.Empty;
+
+        [MinLength(3), MaxLength(200)]
+        public string? Description { get; set; } = String.Empty;
+
+        [MaxLength(200)]
+        public string? ImageUrl { get; set; } = String.Empty;
+
+        public IList<Product>? Products { get; set; }
+    }
+}
---
+++ FoodApp.Menu/DTOs/ProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using FoodApp.Menu.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodApp.Menu.DTOs
+{
+    public class ProductDTO
+    {
+        [Required(ErrorMessage = "The name is required.")]
+        [MinLength(3), MaxLength(200)]
+        public string? Name { get; set; } = String.Empty;
+
+        [MinLength(3), MaxLength(200)]
+        public string? Description { get; set; } = String.Empty;
+
+        [MaxLength(200)]
+        public string? ImageUrl { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "The price is required")]
+        public decimal? Price { get; set; } = 0;
+
+        public IList<Category> Category { get; set; } = [];
+    }
+}
