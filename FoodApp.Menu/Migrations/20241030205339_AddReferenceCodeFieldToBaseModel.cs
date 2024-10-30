bde83ba32fc4fdb4525e8104c838c6af74741cb5
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 17:57:22 -0300

    feat: Atualiza o context para gerar novo migration

---
+++ FoodApp.Menu/Migrations/20241030205339_AddReferenceCodeFieldToBaseModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodApp.Menu.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferenceCodeFieldToBaseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceCode",
+                table: "Products",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceCode",
+                table: "Categories",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenceCode",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ReferenceCode",
+                table: "Categories");
+        }
+    }
+}
