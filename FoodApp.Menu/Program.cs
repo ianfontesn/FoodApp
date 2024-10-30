2578d8795755dfc17dff4eff248f58e3b3110fee
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 14:36:34 -0300

    feat: Adiciona no Program.cs os repositorios e unity of work

--- FoodApp.Menu/Program.cs
+++ FoodApp.Menu/Program.cs
@@ -1,45 +1,50 @@
 
 using FoodApp.Menu.Context;
+using FoodApp.Menu.Repositories;
+using FoodApp.Menu.Repositories.Interfaces;
+using FoodApp.Menu.Repositories.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 
-namespace FoodApp.Menu
+namespace FoodApp.Menu;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        builder.Services.AddTransient<IProductRepository, ProductRepository>();
+        builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+        builder.Services.AddTransient<IUnityOfWork, UnitOfWork>();
+        
+
+
+        //BD
+        var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            //BD
-            var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
