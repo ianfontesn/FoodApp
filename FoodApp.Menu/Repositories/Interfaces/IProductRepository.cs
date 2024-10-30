b02344fa12e490231b94a61c55fd5100ee5e65c6
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 14:34:41 -0300

    feat: Adiciona Product repository e sua interface

---
+++ FoodApp.Menu/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using FoodApp.Menu.Models;
+
+namespace FoodApp.Menu.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+
+        Task<IEnumerable<Product>> GetProductCategorie();
+
+        Task<Product> GetByName(string name);
+
+        Task<Product> GetById(int id);
+
+        Task<Product> Create(Product product);
+
+        Task<Product> Update(Product product);
+
+        Task<Product> Delete(int id);
+    }
+}
---
+++ FoodApp.Menu/Repositories/ProductRepository.cs
@@ -0,0 +1,83 @@
+﻿using FoodApp.Menu.Context;
+using FoodApp.Menu.Helpers.Exceptions.ProductExceptions;
+using FoodApp.Menu.Models;
+using FoodApp.Menu.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodApp.Menu.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            return product;
+        }
+
+        public async Task<Product> Delete(int id)
+        {
+            var Product = await _context.Products
+                .FirstOrDefaultAsync(c => c.Id == id)
+                ?? throw new ProductNotFoundException(id: id);
+
+            _context.Products.Remove(Product);
+
+            return Product;
+        }
+
+        public async Task<IList<Product>> GetAll()
+        {
+            var ProductList = await _context.Products.ToListAsync();
+            return ProductList;
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            var Product = await _context.Products
+                .FirstOrDefaultAsync(c => c.Id == id)
+                ?? throw new DeleteProductException(id);
+
+            _context.Products.Remove(Product);
+            return Product;
+        }
+
+        public async Task<Product> GetByName(string name)
+        {
+            var product = await _context.Products
+                .FirstOrDefaultAsync(p => p.Name == name)
+                ?? throw new ProductNotFoundException(name: name);
+
+            _context.Products.Remove(product);
+            return product;
+        }
+
+        public Task<IEnumerable<Product>> GetProductCategorie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> Update(Product Product)
+        {
+            var existingProduct = await _context.Products
+                .FirstOrDefaultAsync(c => c.Id == Product.Id)
+                ?? throw new ProductNotFoundException(Product.Id);
+
+            _context.Products.Entry(existingProduct).CurrentValues.SetValues(Product);
+
+            return existingProduct;
+
+        }
+
+        Task<IEnumerable<Product>> IProductRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
