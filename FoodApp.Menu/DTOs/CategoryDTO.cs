c3b3499af7424cb8df008401a869361387f23ca0
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 18:13:45 -0300

    feat: Adiciona reference code aos dtos e mapping

--- FoodApp.Menu/DTOs/CategoryDTO.cs
+++ FoodApp.Menu/DTOs/CategoryDTO.cs
@@ -5,6 +5,10 @@ namespace FoodApp.Menu.DTOs
 {
     public class CategoryDTO
     {
+        [Required(ErrorMessage = "The reference code is required.")]
+        [MinLength(1), MaxLength(10)]
+        public string? ReferenceCode { get; set; } = String.Empty;
+
         [Required(ErrorMessage = "The name is required.")]
         [MinLength(3), MaxLength(200)]
         public string? Name { get; set; } = String.Empty;
