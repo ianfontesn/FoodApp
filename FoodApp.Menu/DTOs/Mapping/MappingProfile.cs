4264e642069fa2821bd4ae693225e7645ed9d2a1
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 14:30:01 -0300

    feat: Adiciona MappingProfile para Product e Category

---
+++ FoodApp.Menu/DTOs/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FoodApp.Menu.Models;
+
+namespace FoodApp.Menu.DTOs.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>()
+            .ReverseMap();
+
+        CreateMap<Product , ProductDTO>() 
+            .ReverseMap();
+    }
+}
