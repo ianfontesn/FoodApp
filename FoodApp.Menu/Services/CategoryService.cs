79671f8547cba23f7153df71af68f72c808dcc68
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-10-30 15:38:36 -0300

    feat: Adiciona interface e implementação de category service

---
+++ FoodApp.Menu/Services/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using FoodApp.Menu.DTOs;
+using FoodApp.Menu.Models;
+using FoodApp.Menu.Repositories.Interfaces;
+using FoodApp.Menu.Services.Interfaces;
+
+namespace FoodApp.Menu.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<CategoryDTO> Delete(int id)
+        {
+            var entity = await _categoryRepository.Delete(id);
+            return mapper.Map<CategoryDTO>(entity);
+        }
+
+        public async Task<IList<CategoryDTO>> FindAll()
+        {
+            var entitylist = await _categoryRepository.GetAll();
+            return mapper.Map<IList<CategoryDTO>>(entitylist);
+        }
+
+        public async Task<CategoryDTO> FindById(int categoryId)
+        {
+            var entity = await _categoryRepository.GetById(categoryId);
+            return mapper.Map<CategoryDTO>(entity);
+        }
+
+        public async Task<CategoryDTO> FindByName(string productName)
+        {
+            var entity = await _categoryRepository.GetByName(productName);
+            return mapper.Map<CategoryDTO>(entity);
+        }
+
+        public async Task<CategoryDTO> Register(CategoryDTO categoryDTO)
+        {
+            var entity = await _categoryRepository.Create(mapper.Map<Category>(categoryDTO));
+
+            return mapper.Map<CategoryDTO>(entity);
+        }
+
+        public async Task<CategoryDTO> Update(CategoryDTO categoryDTO)
+        {
+            var entity = await _categoryRepository.Update(mapper.Map<Category>(categoryDTO));
+            return mapper.Map<CategoryDTO>(entity);
+        }
+    }
+}
--- FoodApp.Menu/Services/Interfaces/ICategoryService.cs
+++ FoodApp.Menu/Services/Interfaces/ICategoryService.cs
@@ -1,6 +1,19 @@
-﻿namespace FoodApp.Menu.Services.Interfaces
+﻿using FoodApp.Menu.DTOs;
+
+namespace FoodApp.Menu.Services.Interfaces
 {
     public interface ICategoryService
     {
+        Task<CategoryDTO> Register(CategoryDTO categoryDTO);
+
+        Task<CategoryDTO> Update(CategoryDTO categoryDTO);
+
+        Task<CategoryDTO> Delete(int id);
+
+        Task<CategoryDTO> FindById(int categoryId);
+
+        Task<CategoryDTO> FindByName(string categoryName);
+
+        Task<IList<CategoryDTO>> FindAll();
     }
 }
